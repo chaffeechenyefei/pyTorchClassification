59ff3983ff1b9e86efa28817eaf5ce96e0c437c0
Yefei Chen <>
2019-10-22 16:10:42 +0800

    no message

--- 2class_clf_pytorch/dataset.py
+++ 2class_clf_pytorch/dataset.py
@@ -557,13 +557,13 @@ class TrainDatasetLocationRS(Dataset):
             res_dat = self._df_pair[['duns_number','atlas_location_uuid','label']]
 
         # concate training pair with location/company feature
-        F_res_dat = pd.merge(res_dat, self._df_comp_feat, on='duns_number', how='left').reset_index(drop=True)
-        list_col = list(F_res_dat.columns)
+        F_res_dat = pd.merge(res_dat, self._df_comp_feat, on='duns_number', how='left')
+        list_col = list(self._df_comp_feat.columns)
         list_col = [col for col in list_col if col not in ['duns_number', 'atlas_location_uuid', 'label']]
         FeatComp = F_res_dat[list_col].to_numpy()
 
-        F_res_dat = pd.merge(res_dat, self._df_loc_feat, on='atlas_location_uuid', how='left').reset_index(drop=True)
-        list_col = list(F_res_dat.columns)
+        F_res_dat = pd.merge(res_dat, self._df_loc_feat, on='atlas_location_uuid', how='left')
+        list_col = list(self._df_loc_feat.columns)
         list_col = [col for col in list_col if col not in ['duns_number', 'atlas_location_uuid', 'label']]
         print(list_col)
         FeatLoc = F_res_dat[list_col].to_numpy()
